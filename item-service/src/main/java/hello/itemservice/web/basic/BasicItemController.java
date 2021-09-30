810225d6bdf8cea70f1582a2e4c8e80c453ca48b
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-09-30 23:08:09 +0900

    스프링 MVC 1편 - 스프링 MVC웹 페이지 만들기_상품상세와 상품등록

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -24,6 +24,77 @@ public class BasicItemController {
 		model.addAttribute("items", items);
 		return "basic/items";
 	}
+	
+	//상품상세
+	@GetMapping("/{itemId}")
+	public String item(@PathVariable long itemId, Model model) {
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item", item);
+		return "basic/item";
+	}
+	
+	
+	//상품등록 폼 GET
+	@GetMapping("/add")
+	public String addForm() {
+		
+		return "basic/addForm";
+	}
+	
+	//상품등록 폼 POST
+	//@PostMapping("/add")
+	public String addItemV1(@RequestParam String itemName, @RequestParam int price, @RequestParam Integer quantity, Model model) {
+		
+		Item item = new Item();
+		item.setItemName(itemName);
+		item.setPrice(price);
+		item.setQuantity(quantity);
+		
+		itemRepository.save(item);
+		model.addAttribute("item", item);
+		
+		return "basic/item";
+	}
+	
+	//상품등록 폼 POST - ModelAttribute
+	//@PostMapping("/add")
+	public String addItemV2(@ModelAttribute("item") Item item) {
+		
+		/*@ModelAttribute가 자동으로 만들어줌으로 삭제 가능~
+		 * Item item = new Item();
+		item.setItemName(itemName);
+		item.setPrice(price);
+		item.setQuantity(quantity);*/
+		
+		itemRepository.save(item);
+		
+		//*@ModelAttribute("item")==> item 이름으로 모델에 자동으로 넣어줌 */
+		//model.addAttribute("item", item);
+		
+		return "basic/item";
+	}
+	
+	/**
+	* @ModelAttribute name 생략 가능
+	* model.addAttribute(item); 자동 추가, 생략 가능
+	* 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
+	*/
+	//@PostMapping("/add")
+	public String addItemV3(@ModelAttribute Item item) {
+		itemRepository.save(item);
+		
+		return "basic/item";
+	}
+	
+	/**
+	* @ModelAttribute 자체 생략 가능
+	* model.addAttribute(item) 자동 추가
+	*/
+	@PostMapping("/add")
+	public String addItemV4(Item item) {
+		itemRepository.save(item);
+		return "basic/item";
+	}
 
 	/**
 	 * 테스트용 데이터 추가
